dbb68d6820f32d3007412731b7feb35ccde2550d
ochi <mune.ochi@gmail.com>
2022-05-19 12:40:36 +0900

    rewrite test methods

--- app/src/test/java/com/example/tweetssearch/util/TweetUtilTest.kt
+++ app/src/test/java/com/example/tweetssearch/util/TweetUtilTest.kt
@@ -1,26 +1,33 @@
 package com.example.tweetssearch.util
 
 import com.google.common.truth.Truth.assertThat
+import org.junit.Before
 import org.junit.Test
 
 class TweetUtilTest {
+    private lateinit var tweetUtil: TweetUtil
+
+    @Before
+    fun setup() {
+        tweetUtil = TweetUtil()
+    }
 
     @Test
-    fun convertCreatedAt_success() {
-        val s = TweetUtil().convertCreatedAt("Thu Mar 25 13:50:48 +0000 2021")
+    fun 作成日時たる引数を日本時間の形式に変換する() {
+        val s = tweetUtil.convertCreatedAt("Thu Mar 25 13:50:48 +0000 2021")
         // 時差は９時間とする
         assertThat("2021/03/25 22:50:48").isEqualTo(s)
     }
 
     @Test
-    fun convertCreatedAt_wrongFormat() {
-        val s = TweetUtil().convertCreatedAt("2021/03/25 22:50:48 +0900")
+    fun 作成日時たる引数の形式が誤りの場合に例外の文字列に変換する() {
+        val s = tweetUtil.convertCreatedAt("2021/03/25 22:50:48 +0900")
         assertThat("java.text.ParseException").isEqualTo(s)
     }
 
     @Test
-    fun convertCreatedAt_emptyText() {
-        val s = TweetUtil().convertCreatedAt("")
+    fun 作成日時たる引数が空の場合に例外の文字列に変換する() {
+        val s = tweetUtil.convertCreatedAt("")
         assertThat("java.text.ParseException").isEqualTo(s)
     }
 
