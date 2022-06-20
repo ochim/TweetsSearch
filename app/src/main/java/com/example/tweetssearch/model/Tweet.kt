2962c7b0671b46eb3c9217a8f6e8be1a8e2a7386
ochi <mune.ochi@gmail.com>
2022-06-20 17:22:09 +0900

    add the Keep annotation

--- app/src/main/java/com/example/tweetssearch/model/Tweet.kt
+++ app/src/main/java/com/example/tweetssearch/model/Tweet.kt
@@ -1,10 +1,12 @@
 package com.example.tweetssearch.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import com.squareup.moshi.Json
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Keep
 data class Tweet(
     val text: String,
     @Json(name = "created_at") val createdAt: String,
--- app/src/main/java/com/example/tweetssearch/model/User.kt
+++ app/src/main/java/com/example/tweetssearch/model/User.kt
@@ -1,9 +1,11 @@
 package com.example.tweetssearch.model
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Keep
 data class User(
     val name: String,
     val screen_name: String,
