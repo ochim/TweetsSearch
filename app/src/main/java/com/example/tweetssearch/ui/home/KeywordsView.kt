a999d8032fc916a747fbf01fa68ccb9ccfed22eb
ochi <mune.ochi@gmail.com>
2022-06-22 22:37:01 +0900

    KeywordsList hotfix

--- app/src/main/java/com/example/tweetssearch/ui/home/KeywordsView.kt
+++ app/src/main/java/com/example/tweetssearch/ui/home/KeywordsView.kt
@@ -1,8 +1,10 @@
 package com.example.tweetssearch.ui.home
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -10,6 +12,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.android.material.composethemeadapter.MdcTheme
 
@@ -26,6 +29,7 @@ fun KeywordsList(keywords: List<String>, onItemClick: (String) -> Unit) {
             items(items = keywords) {
                 Text(
                     modifier = Modifier
+                        .padding(start = 8.dp)
                         .fillMaxWidth()
                         .clickable { onItemClick(it) },
                     style = MaterialTheme.typography.h6,
@@ -34,4 +38,14 @@ fun KeywordsList(keywords: List<String>, onItemClick: (String) -> Unit) {
             }
         }
     }
+}
+
+@Preview(name = "Light theme")
+@Preview(name = "Dark theme", uiMode = UI_MODE_NIGHT_YES)
+@Composable
+fun KeywordsListPreview() {
+    KeywordsList(
+        keywords = listOf("#android", "hoge"),
+        onItemClick = {}
+    )
 }
